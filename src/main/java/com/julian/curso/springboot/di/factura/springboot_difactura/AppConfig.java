72d9e36df9015b4ac353e00555da77bd8a7816af
Julian-Mora <felipmora43@gmail.com>
2026-01-13 16:20:04 -0500

    Creacion de Constructores Item , Invoice, ademas de crear Bean de la lista de Item

---
+++ src/main/java/com/julian/curso/springboot/di/factura/springboot_difactura/AppConfig.java
@@ -0,0 +1,25 @@
+package com.julian.curso.springboot.di.factura.springboot_difactura;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.julian.curso.springboot.di.factura.springboot_difactura.models.Item;
+import com.julian.curso.springboot.di.factura.springboot_difactura.models.Product;
+
+@Configuration
+@PropertySource("classpath:data.properties")
+public class AppConfig {
+
+    @Bean
+    List<Item> itemsInvoice(){
+
+        Product p1 = new Product("Camara Sony", 800);
+        Product p2 = new Product("Reloj  Samsung",1200);
+        List<Item> items = Arrays.asList(new Item(p1,2),new Item(p2,4)); // Lista inmutable
+        return items;
+    }
+}
